96a2eed8a27d46bb22b416e62f93d020523429a9
Aniruddh Doki <doki.aniruddh@gmail.com>
2021-09-28 09:27:29 -0400

    a

--- nozzle separation.py
+++ nozzle separation.py
@@ -8,23 +8,32 @@ def water_range(nozzle_separation, spray_radius):
   xi = 0.0
   xf = nozzle_separation
   spray_radius = spray_radius
-  water = list()
+  water = []
   for x in numpy.arange(xi, xf, 0.01):
     left_chord = chord(x, spray_radius, 0)
     right_chord = chord(x, spray_radius, nozzle_separation)
     if left_chord + right_chord >= 0:
       water.append(left_chord + right_chord)
+  # print(water)
   maxmin = max(water) - min(water)
   return maxmin
 
 def optimize(max_nozzle_separation, spray_radius):
-  xi = 0
+  xi = 25
   xf = max_nozzle_separation
-  ranges = list()
-  nozzle_separation_value = list()
-  for x in numpy.arange(xi, xf, 0.01):
+  ranges = []
+  nozzle_separation_value = []
+  for x in numpy.arange(xi + 0.01, xf, 0.01):
+    # print(x)
+    nozzle_separation_value.append(x)
     ranges.append(water_range(x, spray_radius))
-  return nozzle_separation_value[ranges.index(min(ranges))]
+  print(max(ranges))
+  print(ranges.index(max(ranges)))
+  print(x[ranges.index(max(ranges))])
+  # return nozzle_separation_value[ranges.index(min(ranges))]
 
-optimize(25, 25)
+def main():
+  optimize(50, 25)
+  return 0
 
+main()
